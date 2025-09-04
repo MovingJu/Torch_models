7448fe07157c0d361c2c5f9bee1586751e8c1874
MovingJu <baramishe@gmail.com>
2025-09-04 12:30:41 +0900

    .

--- modules/Face_detection/Datasets.py
+++ modules/Face_detection/Datasets.py
@@ -1,40 +1,47 @@
 import torchvision.transforms as transform
 from torch.utils.data import Dataset
 import torch, cv2, pandas as pd
+import numpy as np
 
 class Datasets(Dataset):
     def __init__(self, dataset_path: str, csv_file_name: str, image_path: str, is_train: bool = False, transform: transform.Compose | None = None):
         self.dataset_path = dataset_path
         self.data = pd.read_csv(dataset_path + "/" + csv_file_name)
         self.image_dir = dataset_path + "/" + image_path
-        self.transform = transform
+
+        self.labels = []
+        self.images = []
+        for idx, _ in enumerate(self.data):
+            img = cv2.imread(
+                        self.image_dir + "/" + str(self.data.iloc[idx, 0]), 
+                        cv2.IMREAD_COLOR
+                    )
+            
+            if img is None:
+                raise RuntimeError("Image is not valid!")
+            if transform:
+                img = transform(img)
+
+            self.images.append(img)
+            self.labels.append(
+                torch.tensor([
+                        self.data.iloc[idx, 1],
+                        self.data.iloc[idx, 2],
+                        self.data.iloc[idx, 3],
+                        self.data.iloc[idx, 4]
+                    ],
+                    dtype=torch.float
+                )
+            )
+        return
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
 
-        label = torch.tensor(
-            [
-                self.data.iloc[idx, 1],
-                self.data.iloc[idx, 2],
-                self.data.iloc[idx, 3],
-                self.data.iloc[idx, 4]
-            ],
-            dtype=torch.float
-        )
-        label = torch.nn.Sigmoid()(label)
-
-        file_name = self.image_dir + "/" + str(self.data.iloc[idx, 0])
-        temp = cv2.imread(file_name, cv2.IMREAD_COLOR)
-
-        if temp is None:
-            import numpy as np
-            result = np.zeros((1, 1, 3), dtype=np.uint8)
-            return result, (-1, -1, -1, -1)
-
-        if self.transform:
-            temp = self.transform(temp)
+        temp = self.images[idx]
+        label = self.labels[idx]
 
         return temp, label
 
